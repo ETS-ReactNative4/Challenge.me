70c5b14e16661ea511e6da325dda6b9e05a99779
Amara Obasi <aobasi4@gmail.com>
2022-04-20 08:03:58 -0400

    new backend

---
+++ challengeme/theme.js
@@ -0,0 +1,10 @@
+import { DefaultTheme} from "react-native-paper";
+
+export const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: '#b434eb',
+        secondary: '#397d46',
+    },
+}
